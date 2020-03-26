740e8294cad76c2b252c5793d9da5f49dcccea15
Kirill Funtov <funtaps@gmail.com>
2020-03-27 01:37:49 +0300

    mycs

--- install.bash
+++ install.bash
@@ -18,32 +18,32 @@ mkdir -p /opt/hlds
 /opt/steam/steamcmd.sh +login $steam_user $steam_password +force_install_dir /opt/hlds +app_update 90 validate +quit
 mkdir -p ~/.steam && ln -s /opt/hlds ~/.steam/sdk32
 ln -s /opt/steam/ /opt/hlds/steamcmd
-cp files/steam_appid.txt /opt/hlds/steam_appid.txt
-cp hlds_run.sh /bin/hlds_run.sh
+cp ~/mycs/files/steam_appid.txt /opt/hlds/steam_appid.txt
+cp ~/mycs/hlds_run.sh /bin/hlds_run.sh
 
 # Add default config
-cp files/server.cfg /opt/hlds/cstrike/server.cfg
+cp ~/mycs/files/server.cfg /opt/hlds/cstrike/server.cfg
 
 # Add maps
-cp maps/* /opt/hlds/cstrike/maps/
-cp files/mapcycle.txt /opt/hlds/cstrike/mapcycle.txt
+cp ~/mycs/maps/* /opt/hlds/cstrike/maps/
+cp ~/mycs/files/mapcycle.txt /opt/hlds/cstrike/mapcycle.txt
 
 # Install metamod
 mkdir -p /opt/hlds/cstrike/addons/metamod/dlls
 curl -sqL "http://prdownloads.sourceforge.net/metamod/metamod-$metamod_version-linux.tar.gz?download" | tar -C /opt/hlds/cstrike/addons/metamod/dlls -zxvf -
-cp files/liblist.gam /opt/hlds/cstrike/liblist.gam
+cp ~/mycs/files/liblist.gam /opt/hlds/cstrike/liblist.gam
 # Remove this line if you aren't going to install/use amxmodx and dproto
-cp files/plugins.ini /opt/hlds/cstrike/addons/metamod/plugins.ini
+cp ~/mycs/files/plugins.ini /opt/hlds/cstrike/addons/metamod/plugins.ini
 
 # Install dproto
 mkdir -p /opt/hlds/cstrike/addons/dproto
-cp files/dproto_i386.so /opt/hlds/cstrike/addons/dproto/dproto_i386.so
-cp files/dproto.cfg /opt/hlds/cstrike/dproto.cfg
+cp ~/mycs/files/dproto_i386.so /opt/hlds/cstrike/addons/dproto/dproto_i386.so
+cp ~/mycs/files/dproto.cfg /opt/hlds/cstrike/dproto.cfg
 
 # Install AMX mod X
 curl -sqL "http://www.amxmodx.org/release/amxmodx-$amxmod_version-base-linux.tar.gz" | tar -C /opt/hlds/cstrike/ -zxvf -
 curl -sqL "http://www.amxmodx.org/release/amxmodx-$amxmod_version-cstrike-linux.tar.gz" | tar -C /opt/hlds/cstrike/ -zxvf -
-cp files/maps.ini /opt/hlds/cstrike/addons/amxmodx/configs/maps.ini
+cp ~/mycs/files/maps.ini /opt/hlds/cstrike/addons/amxmodx/configs/maps.ini
 
 # Add admin
 echo "\"STEAM_0:0:40763629\" \"\"  \"abcdefghijklmnopqrstu\" \"ce\"" >> "/opt/hlds/cstrike/addons/amxmodx/configs/users.ini"
